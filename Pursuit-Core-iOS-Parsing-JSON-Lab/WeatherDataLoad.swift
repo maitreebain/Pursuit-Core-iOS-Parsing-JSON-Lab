acffd7f76201d0bde9984d400172175b1ba8859b
maitreebain <maitreebain@gmail.com>
2019-11-25 17:19:46 -0500

    working on weather array info, og extension is done

---
+++ Pursuit-Core-iOS-Parsing-JSON-Lab/WeatherDataLoad.swift
@@ -0,0 +1,52 @@
+//
+//  WeatherDataLoad.swift
+//  Pursuit-Core-iOS-Parsing-JSON-Lab
+//
+//  Created by Maitree Bain on 11/25/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherDataLoad: Codable {
+    let results: [WeatherInfo]
+}
+
+struct WeatherInfo: Codable {
+    var name: String
+    var weather: [WeatherArrayData]
+}
+
+struct WeatherArrayData: Codable {
+    var id: Int
+    var main: String
+    var description: String
+}
+
+extension WeatherDataLoad {
+    
+    static func getWeather() -> [WeatherInfo] {
+        var weatherInfo = [WeatherInfo]()
+    
+        guard let fileURL = Bundle.main.url(forResource: "WeatherData", withExtension: "json") else {
+            fatalError("could not find weather data file")
+        }
+        
+        do {
+            let data = try Data.init(contentsOf: fileURL)
+            
+            let weatherType = try JSONDecoder().decode(WeatherDataLoad.self, from: data)
+            weatherInfo = weatherType.results
+        }
+        catch {
+            fatalError("error found: \(error)")
+        }
+        
+        return weatherInfo
+}
+}
+
+extension WeatherInfo {
+    
+    
+}
