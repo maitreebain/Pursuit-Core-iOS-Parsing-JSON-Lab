5915f14d296e25c1ff8481a3bd78c498d4cdb0ad
maitreebain <maitreebain@gmail.com>
2019-11-25 16:54:07 -0500

    Initial Commit

---
+++ Pursuit-Core-iOS-Parsing-JSON-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pursuit-Core-iOS-Parsing-JSON-Lab
+//
+//  Created by Maitree Bain on 11/25/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
