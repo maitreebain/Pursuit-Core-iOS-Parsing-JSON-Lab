7eae5b9b1f173b1b03af64ffea11ea551dba1a26
maitreebain <maitreebain@gmail.com>
2019-12-02 23:01:57 -0500

    done calling data

---
+++ Pursuit-Core-iOS-Parsing-JSON-Lab/RandomUserDataLoad.swift
@@ -0,0 +1,57 @@
+//
+//  RandomUserDataLoad.swift
+//  Pursuit-Core-iOS-Parsing-JSON-Lab
+//
+//  Created by Maitree Bain on 12/2/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import Foundation
+
+struct RandomUserDataLoad: Codable {
+    let results: [UserInfo]
+}
+
+struct UserInfo: Codable {
+    let name: UserName
+    let location: UserLocation
+    let email: String
+    let phone: String
+}
+
+struct UserName: Codable {
+    let first: String
+    let last: String
+}
+
+struct UserLocation: Codable {
+    let street: UserStreetInfo
+}
+
+struct UserStreetInfo: Codable {
+    let number: Double
+    let name: String
+}
+
+extension RandomUserDataLoad {
+    
+    static func getUserInfo() -> [UserInfo] {
+        var userInfo = [UserInfo]()
+    
+        guard let fileURL = Bundle.main.url(forResource: "RandomUserData", withExtension: "json") else {
+            fatalError("could not find color Data file")
+        }
+        
+        do {
+            let data = try Data.init(contentsOf: fileURL)
+            
+            let userData = try JSONDecoder().decode(RandomUserDataLoad.self, from: data)
+            userInfo = userData.results
+        } catch {
+            fatalError("no data found")
+        }
+        
+        return userInfo
+    }
+    
+}
---
+++ Pursuit-Core-iOS-Parsing-JSON-Lab/RandomUserViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RandomUserViewController.swift
+//  Pursuit-Core-iOS-Parsing-JSON-Lab
+//
+//  Created by Maitree Bain on 12/2/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class RandomUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
